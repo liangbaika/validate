4a5e182b1bbcbd5b885146f585114933b02782b4
liqiang <1144388620@qq.com>
2021-02-05 10:01:26 +0800

    test

--- src/test/java/com/github/liangbaika/validate/test/Tests.java
+++ src/test/java/com/github/liangbaika/validate/test/Tests.java
@@ -1,6 +1,7 @@
 package com.github.liangbaika.validate.test;
 
 import com.github.liangbaika.validate.core.ValidateBuilder;
+import org.junit.Test;
 
 import static com.github.liangbaika.validate.enums.Check.*;
 
@@ -12,34 +13,20 @@ import static com.github.liangbaika.validate.enums.Check.*;
  * @date 2021/1/29 21:44
  */
 public class Tests {
-    public static void main(String[] args) {
 
+    @Test
+    public void testValidateBuilder() {
         ValidateBuilder validateBuilder = ValidateBuilder.build();
         int failedCounts = validateBuilder
-                .vali(ne, "3", "3","不能等于")
+                .vali(ne, "3", "3", "不能等于")
                 .vali(Chinese, "测试中文")
                 .vali(isBirthdaystr, "1992-12-09")
                 .vali(isUrl, "https://baidu.com")
                 .doCheck()
                 .getFailedCounts();
-        System.out.println(failedCounts);
-        System.out.println(validateBuilder.getSuccedCounts());
-        System.out.println(validateBuilder.getFailedMsgs());
-        System.out.println(validateBuilder.isPassed());
-
-
-        //重复使用 validateBuilder  先调用clear方法
-        int failedCounts2 = validateBuilder
-                .clear()
-                .vali(isGeneral, "ssa2_")
-                .vali(Chinese, "测试中文")
-                .vali(isBirthdaystr, "1992/12/09")
-                .vali(isUrl, "http://baidu.com")
-                .doCheck()
-                .ifNotPassedThrowException()
-                .getFailedCounts();
-        System.out.println(failedCounts2);
-        System.out.println(validateBuilder.getSuccedCounts());
+        assert failedCounts == 1;
+        assert validateBuilder.getSuccedCounts() == 3;
 
     }
+
 }
